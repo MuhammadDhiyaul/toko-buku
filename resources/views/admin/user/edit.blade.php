930edaf17df393a9877a9a27129aa59d9ac6131b
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2022-11-04 17:23:41 +0700

    TSA_Sertifikasi

---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title', 'View Users')
+
+@section('content')
+
+<div class="container-fluid px-4">
+
+    <div class="card mt-4">
+        <div class="card-header">
+            <h4>View Users</h4>
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+
+</div>
+    
+@endsection
